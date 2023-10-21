b185f9cde38a9e25476a875e5f7f433f5d861b57
habacuc.eth <habacuc.vera@gmail.com>
2023-10-21 11:38:31 -0600

    boost card

--- packages/nextjs/pages/boost.tsx
+++ packages/nextjs/pages/boost.tsx
@@ -1,38 +1,91 @@
-import type { NextPage } from "next";
+import React, { useState } from "react";
 
-const Donate: NextPage = () => {
+const DepositWithdrawComponent: React.FC = () => {
+  const [isDeposit, setIsDeposit] = useState(true);
+  const [amount, setAmount] = useState(0);
+
   return (
-    <div className="m-auto p-8 rounded-md gap-3 grid text-center font-bau ">
-      <div className="bg-gradient-to-b from-black/75 to-gray-500 p-3 rounded-md ">
-        <div className="grid items-center gap-3 pb-3">
-          <div className="text-3xl text-transparent bg-clip-text bg-gradient-to-l from-dai to-sdai">Boost</div>
-          <div className="text-white text-4xl">1,000.35 DAI</div>
-          <div className="text-sdai text-lg">(975 sDAI)</div>
+    <div className="m-auto p-8 rounded-md gap-3 grid text-center font-bau">
+      <div className="bg-gradient-to-b from-black/75 to-gray-500 p-3 rounded-md">
+        <div className="grid items-center gap-3">
+          <div className="text-3xl text-white">
+            <span className="text-dai">Deposit DAI </span>to share your <span className="text-sdai">sDAI yield</span>
+          </div>
+          <div className="flex space-x-2 mx-auto bg-white rounded-md">
+            <button
+              onClick={() => setIsDeposit(true)}
+              className={`px-4 py-2 rounded-md ${isDeposit ? "bg-dai text-white" : " text-dai"}`}
+            >
+              Deposit
+            </button>
+            <button
+              onClick={() => setIsDeposit(false)}
+              className={`px-4 py-2 rounded-md ${!isDeposit ? "bg-sdai text-white" : " text-sdai"}`}
+            >
+              Withdraw
+            </button>
+          </div>
         </div>
+        {isDeposit ? (
+          <>
+            <p className="text-dai mb-2">30.00 DAI available</p>
+            <div className="relative mb-3">
+              <input
+                type="number"
+                value={amount}
+                onChange={e => setAmount(parseFloat(e.target.value))}
+                className="input input-ghost input-bordered border-3 border-white w-full max-w-xs pl-8 focus:bg-white/20"
+              />
+              <button onClick={() => setAmount(30)} className="absolute right-28 top-3 cursor-pointer text-dai">
+                MAX
+              </button>
+            </div>
+            <div className="grid">
+              <div className="text-dai px-3 py-2 flex mx-auto items-center">
+                <div className="text-xl font-bold px-3">Your will deposit</div>
+                <div className="text-2xl text-white">0 DAI</div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <>
+            <p className="text-sdai mb-2">28.00 sDAI available</p>
+            <div className="relative mb-3">
+              <input
+                type="number"
+                value={amount}
+                onChange={e => setAmount(parseFloat(e.target.value))}
+                className="input input-ghost input-bordered border-3 border-white w-full max-w-xs pl-8 focus:bg-white/20"
+              />
+              <button onClick={() => setAmount(28)} className="absolute right-28 top-3 cursor-pointer text-sdai">
+                MAX
+              </button>
+            </div>
+            <div className="grid grid-cols-2 max-w-xs mx-auto pb-2">
+              <div className="text-dai py-2 flex mx-auto items-center">
+                <div className="text-white px-3">0 DAI</div>
+                <div className="font-bold">to withdraw </div>
+              </div>
+              <div className="text-sdai py-2 rounded-in flex mx-auto">
+                <div className="text-white px-3">0 sDAI</div>
+                <div className="font-bold">to transfer</div>
+              </div>
+            </div>
+          </>
+        )}
 
-        <div className="grid grid-cols-2 gap-3 ">
-          <div className="grid gap-3 border-2 border-sdai rounded-in">
-            <div className="text-sdai p-3 rounded-in">
-              <p className="text-xl font-bold">Donated</p>
-              <p className="text-2xl text-white">900 sDAI</p>
-              <p>
-                from <span>135</span> donors
-              </p>
-            </div>
-          </div>
-          <div className="grid gap-3 border-2 border-dai rounded-in">
-            <div className="text-dai p-4 rounded-in">
-              <p className="text-xl font-bold">Boosted</p>
-              <p className="text-2xl text-white">100 sDAI</p>
-              <p>
-                from <span>8</span> donors
-              </p>
-            </div>
-          </div>
-        </div>
+        <button
+          className={
+            isDeposit
+              ? "bg-white/20 text-dai mx-auto w-full max-w-xs rounded-md text-2xl py-3 hover:bg-dai hover:text-white"
+              : "bg-white/20 text-sdai mx-auto w-full max-w-xs rounded-md text-2xl py-3 hover:bg-sdai hover:text-white"
+          }
+        >
+          {isDeposit ? "Deposit" : "Withdraw"}
+        </button>
       </div>
     </div>
   );
 };
 
-export default Donate;
+export default DepositWithdrawComponent;
