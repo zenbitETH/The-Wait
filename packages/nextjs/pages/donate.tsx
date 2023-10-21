4386a267fc3133ae30a1f4999c2443a19f7331ca
habacuc.eth <habacuc.vera@gmail.com>
2023-10-21 10:40:35 -0600

    Button update

--- packages/nextjs/pages/donate.tsx
+++ packages/nextjs/pages/donate.tsx
@@ -14,28 +14,30 @@ const Boost: NextPage = () => {
           <input
             type="number"
             placeholder="0.00"
-            className="input input-ghost input-bordered border-3 border-white w-full max-w-xs"
+            className="input input-ghost input-bordered border-3 border-white w-full max-w-xs pl-8 focus:bg-white/20 "
           />
-          <div className="absolute right-10 top-3">MAX</div>
+          <div className="absolute right-16 top-3 cursor-pointer">MAX</div>
         </div>
 
         <div className="grid  gap-3 pb-3 max-w-xs mx-auto">
-          <div className="grid gap-3 border-2 border-dai rounded-in">
-            <div className="text-dai px-3 rounded-in flex mx-auto">
-              <p>✅</p>
-              <p className="text-xl font-bold px-3">Your will deposit</p>
-              <p className="text-2xl text-white">0 DAI</p>
+          <div className="grid  border-2 border-dai/70 rounded-md">
+            <div className="text-dai px-3 py-2 flex mx-auto items-center">
+              <span>✅</span>
+              <div className="text-xl font-bold px-3">Your will deposit</div>
+              <div className="text-2xl text-white">0 DAI</div>
             </div>
           </div>
-          <div className="grid gap-3 border-2 border-sdai rounded-in">
-            <div className="text-sdai px-3 rounded-in flex mx-auto">
-              <p>⏳</p>
-              <p className="text-xl font-bold px-3">Your will transfer</p>
-              <p className="text-2xl text-white">0 sDAI</p>
+          <div className="grid gap-3 border-2 border-sdai/70 rounded-md">
+            <div className="text-sdai px-3 py-2 rounded-in flex mx-auto">
+              <div>⏳</div>
+              <div className="text-xl font-bold px-3">Your will transfer</div>
+              <div className="text-2xl text-white">0 sDAI</div>
             </div>
           </div>
         </div>
-        <button className="bg-sdai mx-auto w-full max-w-xs rounded-md text-2xl py-3">Donate</button>
+        <button className="bg-white/20 text-sdai mx-auto w-full max-w-xs rounded-md text-2xl py-3 hover:bg-sdai hover:text-white">
+          Donate
+        </button>
       </div>
     </div>
   );
